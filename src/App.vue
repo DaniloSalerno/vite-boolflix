7fdfb99d4a0f24df2e038fae5983929e88ff4d6c
DaniloSalerno <salerno.danilo@hotmail.it>
2023-09-28 13:03:08 +0200

    add state.js

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
 <script>
+
+import { state } from './state.js';
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      state
+    }
+  },
+  components: {
+  },
+  created() {
+    this.state.fetchMovieByName(this.state.urlBase + this.state.filmName)
+  }
+}
 </script>
 
 <template></template>
--- src/state.js
+++ src/state.js
@@ -1,5 +1,23 @@
 import { reactive } from "vue";
+import axios from 'axios';
+
 
 export const state = reactive({
 
+    urlBase: 'https://api.themoviedb.org/3/search/movie?api_key=037b7cd8e55255b918dfc215381aac28&query=',
+    filmName: 'matrix',
+    fetchMovieByName(url) {
+
+        axios
+            .get(url)
+            .then(response => {
+
+                console.log(response.data.results);
+
+            })
+            .catch(error => {
+                console.error(error.message);
+            })
+
+    },
 })
