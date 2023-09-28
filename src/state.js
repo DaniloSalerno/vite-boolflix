3cbb254970a1942c52f06c3380301b6bae0d64a6
DaniloSalerno <salerno.danilo@hotmail.it>
2023-09-28 13:36:20 +0200

    milestone 1
    add search input,button and function for see le film list by name after click on button

--- src/state.js
+++ src/state.js
@@ -5,18 +5,24 @@ import axios from 'axios';
 export const state = reactive({
 
     urlBase: 'https://api.themoviedb.org/3/search/movie?api_key=037b7cd8e55255b918dfc215381aac28&query=',
-    filmName: 'matrix',
+
+    filmSearched: '',
+
+    results: '',
+
+
+
     fetchMovieByName(url) {
 
         axios
             .get(url)
             .then(response => {
 
-                console.log(response.data.results);
+                this.results = response.data.results
 
             })
             .catch(error => {
-                console.error(error.message);
+                console.error(error);
             })
 
     },
